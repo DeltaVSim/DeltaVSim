36a5a809cd17d5d113e6ecf89bf1c567d3b7e6ef
sairit <sai.ritvik67@gmail.com>
2025-02-08 13:28:40 -0500

    Initial setup for Logic

--- public/logic.js
+++ public/logic.js
@@ -1,3 +1,57 @@
 class Logic {
+    constructor() {
+        this.data = {
+            fuel: 0,
+            thrust: 0,
+            force: 0,
+            gravity: 0,
+            over: false
+        }
+    }
 
+    calculateForce(rocket) {
+        m = rocket.getMass();
+        g = this.getGravity();
+        t = this.getThrust();
+        w = m * g;
+        this.data.force = t - w;
+    }
+
+    getForce() {
+        return this.data.force;
+    }
+
+    setGravity(gravity) {
+        this.data.gravity = gravity
+    }
+
+    getGravity() {
+        return this.data.gravity;
+    }
+
+    setFuel(fuel) {
+        this.data.fuel = fuel;
+    }
+
+    getFuel() {
+        return this.data.fuel;
+    }
+
+    setThrust(thrust1, thrust2) {
+        this.data.thrust = thrust1 + thrust2;
+    }
+
+    getThrust() {
+        return this.data.thrust;
+    }
+
+    setOver(threshold, force) {
+        if (Math.abs(force) > threshold) {
+            this.data.over = true;
+        }
+    }
+
+    isOver() {
+        return this.data.over;
+    }
 }
