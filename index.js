ac2726d454d7b194eb1d8b3c7f4827b23829d169
Robert <robertaburdsall@gmail.com>
2025-02-09 13:40:12 -0500

    remade routes for node server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.static('public'))
 port = 80
 
 app.listen(port, () => {
@@ -10,6 +11,14 @@ app.listen(port, () => {
   app.get('/', (req, res) => {
     console.log('/ made from: ' + req.ip)
     res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  app.get('/levels', (req, res) => {
+    console.log('/levels made from: ' + req.ip)
+    res.sendFile(path.join(__dirname, 'public/levels.html'));
+  });
+  app.get('/simulator', (req, res) => {
+    console.log('/simulator made from: ' + req.ip)
+    res.sendFile(path.join(__dirname, 'public/simulator.html'));
   });
 
   app.get('/getlevel', (req, res) => {
