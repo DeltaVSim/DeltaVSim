c7d8d6b5ef4b0489ee8af43620bf85a9f9b1bcf3
Robert <robertaburdsall@gmail.com>
2025-02-09 14:29:21 -0500

    minor changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static('public'))
-port = 80
+port = 3000
 
 app.listen(port, () => {
     console.log(`DeltaVSim started on ${port}`);
